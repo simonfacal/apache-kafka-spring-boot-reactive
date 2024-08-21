940c723613df9217b116b6e9fb05fb5cd946ab3d
simonfacal <simofacal@live.com.ar>
2024-08-21 11:07:44 -0300

    Project created

---
+++ producer/src/main/java/com/simon/wikimedia/producer/producer/WikimediaProducer.java
@@ -0,0 +1,17 @@
+package com.simon.wikimedia.producer.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class WikimediaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String msg) {
+        kafkaTemplate.send("wikimedia-stream",msg);
+    }
+}
